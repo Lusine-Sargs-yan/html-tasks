142a22401434f95570c6bc1041afaa101cd64f76
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-03-12 16:26:11 +0400

    about DOM events

--- lectureJSandDom/events/index.js
+++ lectureJSandDom/events/index.js
@@ -1,7 +1,28 @@
-const btn = document.querySelector('.btn');
-console.log(btn);
-// const desc = document.querySelector('.desc');
-// btn.addEventListener('click', () => {
-//   console.log(desc);
-//   desc.style.color = `rgb(${})`
-// })
+const buttn = document.querySelector(".btn");
+console.log(buttn);
+const descriptions = document.querySelector(".description");
+const modalButton = document.querySelector('.modal-btn');
+const modalBody = document.querySelector('.modal');
+
+
+//with  declaring variable
+// const changeColor = buttn.addEventListener("click", () => {
+//   console.log('clicked', 'aaaa');
+  
+// });
+// console.log(changeColor);
+
+
+buttn.addEventListener("click", (target) => {
+  //console.log(target, 'clicked');
+  console.log('clicked', 'count');
+  console.log(descriptions);
+  descriptions.style.color = `rgb(${Math.random() * 100}, ${Math.random() * 100}, ${
+    Math.random() * 100
+  })`;//'red';
+});
+
+modalButton.addEventListener('click', (target) =>  {
+  console.log(target);//target -> թե որտեղ է click եղել
+  modalBody.classList.toggle('modal-active');
+});
